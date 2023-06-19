f4c193fa09f9ba599027048518c5c739cb14095b
WandaSimeoli <wandasimeoli@gmail.com>
2023-06-19 20:49:57 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-const list = ['Latte','Biscotti','Cereali','Fette biscottate','Nutella'];
+const list = ['Latte','Biscotti','Cereali','Fette biscottate','Nutella', 'Miele'];
 
-while {
+const spesa = document.getElementById('spesa');
+
+let j = 0
+while (j < list.length) {
+   console.log('Prodotto', list[j]);
+
+   const newLi = document.createElement('li');
+   
+   spesa.append(newLi);
     
-}
+   newLi.innerHTML = list[j];
+
+    j++;  
+} 
